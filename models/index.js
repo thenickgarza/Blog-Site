55c2021c4f7bb98d7912511e2e9afc695bd973f1
Nick Garza <thenickgarza14@gmail.com>
2022-02-18 10:16:52 -0600

    added homepage, login, and single-post.handlebars w/ some template code

--- models/index.js
+++ models/index.js
@@ -2,5 +2,14 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post')
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    foreignKey: 'user_id'
+});
+
+
 
 module.exports = { User, Comment, Post };
